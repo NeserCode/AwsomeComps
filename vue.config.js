9dd12ed1d93f99fecc80f74969d9eb7c88894b55
✨NeserCode <nesercode@gmail.com>
2022-11-07 19:02:51 +0800

    update public Path

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production' ? '/AwsomeComps/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
